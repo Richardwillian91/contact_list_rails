cbed16c6622fcec286d73e672ad97f44631fb76d
Richard <richardwillian166@hotmail.com>
2022-08-18 16:49:40 -0300

    Páginas estáticas

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,12 @@ class StaticPagesController < ApplicationController
     def index
         @nome = "Richard"
     end
+
+    def sobre
+    
+    end
+
+    def contato
+
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root to: "static_pages#index"
+
+  get 'sobre', to: "static_pages#sobre"
+  get 'contato', to: "static_pages#contato"
+
+  
 end
